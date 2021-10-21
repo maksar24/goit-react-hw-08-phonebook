9c3b2e89f676bcdea63f51992505444f11af4807
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-10-21 16:57:49 +0200

    Updates

---
+++ static/js/main.cbc137ff.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.cbc137ff.chunk.js.map
